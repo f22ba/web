78ecca392738960d0d47d821d7e0539933c9821c
F22BA110 <114005865+f22ba@users.noreply.github.com>
2023-02-09 15:40:40 +0900

    Delete cidweb directory

--- cidweb/cidweb/js/login.js
+++
@@ -1,5 +0,0 @@
-function push_c_btn() {
-    let idText = document.getElementById("id").value;
-    let pwText = document.getElementById("pass").value;
-    location.href = "https://f22ba.github.io/web/" + idText + "/" + pwText + ".html";
-}
