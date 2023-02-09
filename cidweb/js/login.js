3daf4ab89fbe74dbe1dd3173d0a17376594f503f
F22BA110 <114005865+f22ba@users.noreply.github.com>
2023-02-09 15:41:43 +0900

    Add files via upload

---
+++ cidweb/js/login.js
@@ -0,0 +1,5 @@
+function push_c_btn() {
+    let idText = document.getElementById("id").value;
+    let pwText = document.getElementById("pass").value;
+    location.href = "https://f22ba.github.io/web/" + idText + "/" + pwText + ".html";
+}
