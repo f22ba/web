72ea746c75669b60009b2c09d0847ea27ea18e90
F22BA110 <114005865+f22ba@users.noreply.github.com>
2023-02-09 17:10:37 +0900

    Add files via upload

--- cidweb/js/login.js
+++ cidweb/js/login.js
@@ -1,5 +1,22 @@
+
 function push_c_btn() {
+    music = new Audio("se/jyumon.wav");
+    music.play();
     let idText = document.getElementById("id").value;
     let pwText = document.getElementById("pass").value;
     location.href = "https://f22ba.github.io/web/" + idText + "/" + pwText + ".html";
 }
+function se_play1() {
+    // idText = document.getElementById("id").value;
+    // pwText = document.getElementById("pass").value;
+    music = new Audio("se/komento.mp3");
+    music.play();
+    setTimeout(se_play2,2000);
+}
+function se_play2() {
+    music = new Audio("se/jyumon.wav");
+    music.play();
+    // idText = document.getElementById("id").value;
+    // pwText = document.getElementById("pass").value;
+    setTimeout(push_c_btn,2000);
+} 
